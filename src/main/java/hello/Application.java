79d1c184230f72c75347260b8a72f0f7caaeef69
vimcmd <vim.cmd@gmail.com>
2016-06-13 00:30:50 +0500

    created SpringBootApplication class

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,51 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.util.FileSystemUtils;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import java.io.File;
+
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+    @Bean // Strictly speaking this bean is not necessary as boot creates a default
+    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
+    }
+
+    public static void main(String[] args) {
+        // clean out activemq data from previous run
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+
+        // launch the application
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // send a message
+        MessageCreator messageCreator = new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage("ping!");
+            }
+        };
+
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        System.out.println("Sending new message");
+        jmsTemplate.send("mailbox-destination", messageCreator);
+    }
+
+}
